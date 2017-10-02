3cba5bd444ed639805373bb02e3a972513ebf90d
awathas <31712439+awathas@users.noreply.github.com>
2017-10-01 18:07:12 -0700

    update

--- main.cpp
+++ main.cpp
@@ -1,6 +1,8 @@
 #include <iostream>
 #include <string>
 #include <fstream>
+#include <map>
+#include <iomanip>
 
 
 using namespace std;
@@ -8,6 +10,20 @@ using namespace std;
 class Lexer {
 private:
 	int state_table[40][25] = { 1 };
+	map<string, int> keyWord;
+
+};
+
+struct Token {
+	int start = -1;
+	int id;
+	string lexme;
+
+	Token() {
+		start = -1;
+		id = -1;
+		lexme = "";
+	}
 };
 
 int main()
