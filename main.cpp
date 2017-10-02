407c252b44eaed4ed4109c3afa4d1ddd5faa2ca1
awathas <31712439+awathas@users.noreply.github.com>
2017-10-01 17:58:29 -0700

    created main.cpp
    contains lexer class

---
+++ main.cpp
@@ -0,0 +1,20 @@
+#include <iostream>
+#include <string>
+#include <fstream>
+
+
+using namespace std;
+
+class Lexer {
+private:
+	int state_table[40][25] = { 1 };
+};
+
+int main()
+{
+
+
+	cout << endl;
+	system("pause");
+	return 0;
+}
