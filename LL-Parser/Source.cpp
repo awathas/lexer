5f2f8f79ace1e8f1a2eda7f8d67ac09d985f1e1f
Jeffrey Liv <jeffroliv@hotmail.com>
2017-11-04 23:07:23 -0700

    Update Source.cpp

--- LL-Parser/Source.cpp
+++ LL-Parser/Source.cpp
@@ -7,17 +7,17 @@
 * @author Anthony Nguyen, Sae Hun Kim
 */
 
-#include "a1parser.h"
-#include "a2lexer.h"
+#include "Parser.h"
+#include "Lexer.h"
 
 int main() {
-	A2Lexer lexer;
+	Lexer lexer;
 
 	//Lexer will initiate and start storing token ID's into the queue.
 	lexer.execute();
-	
+
 	//Parser will receive a token queue and set it to its member variable token_queue
-	A1Parser parser(lexer.getTokenQueue());
+	Parser parser(lexer.getTokenQueue());
 
 	//Parser will start parsing with the new queue that was given from lexer.getTokenQueue();
 	parser.start_parse();
